a915252d4f3e01b58c96552159ed9a0a9d72b6a0
Fieldname <martynas.ven@protonmail.com>
2022-02-02 23:44:30 +0200

    0202

---
+++ 0202/0202/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<int> gradingStudents(List<int> grades)
+    {
+        var roundedGrades = new List<int>();
+        var tempGrade = 0;
+        foreach (var grade in grades)
+        {
+            if (grade >= 38 && grade % 5 > 2)
+            {
+                tempGrade = grade + 5 - grade % 5;
+                roundedGrades.Add(tempGrade);
+                continue;
+            }
+            roundedGrades.Add(grade);
+        }
+        return roundedGrades;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> grades = new List<int>();
+
+        for (int i = 0; i < gradesCount; i++)
+        {
+            int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
+            grades.Add(gradesItem);
+        }
+
+        List<int> result = Result.gradingStudents(grades);
+
+        Console.WriteLine(String.Join("\n", result));
+    }
+}
